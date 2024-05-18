a1185656219b598d4b7bdb99ea9fac763939ee57
Jonathan Alba Videira <jonathan.videira@gmail.com>
2024-05-18 00:57:51 -0300

    New sonar instance

--- src/main/java/br/com/ajvideira/algafood/api/controller/HomeController.java
+++ src/main/java/br/com/ajvideira/algafood/api/controller/HomeController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/")
+@RestController
 public class HomeController {
 
     @GetMapping
