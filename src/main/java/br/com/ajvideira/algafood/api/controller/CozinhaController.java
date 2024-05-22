9c5c11a204b59a6ca4ae71cdb71b7383d4919b65
Jonathan Alba Videira <jonathan.videira@gmail.com>
2024-05-21 23:00:06 -0300

    Creating method getAll in EstadoController

--- src/main/java/br/com/ajvideira/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/ajvideira/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package br.com.ajvideira.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class CozinhaController {
         this.cozinhaRepository = cozinhaRepository;
     }
 
-    @GetMapping
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public List<Cozinha> getAll() {
         return cozinhaRepository.findAll();
     }
--- src/main/java/br/com/ajvideira/algafood/api/controller/EstadoController.java
+++ src/main/java/br/com/ajvideira/algafood/api/controller/EstadoController.java
@@ -2,6 +2,8 @@ package br.com.ajvideira.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ public class EstadoController {
         this.estadoRepository = estadoRepository;
     }
 
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public List<Estado> getAll() {
         return this.estadoRepository.findAll();
     }
