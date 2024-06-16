b594ca7ea19352ae7f13f1def7ee57e1845d0f00
Jonathan Alba Videira <jonathan.videira@gmail.com>
2024-06-16 16:40:33 -0300

    Including model Produto

---
+++ src/main/java/br/com/ajvideira/algafood/domain/model/Produto.java
@@ -0,0 +1,42 @@
+package br.com.ajvideira.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Produto {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Restaurante restaurante;
+
+}
